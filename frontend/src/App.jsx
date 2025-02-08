419c41923b43053f6c10bbe97967e84e54501d44
Shahriar Ahmed <shahriarabiddut@gmail.com>
2025-02-08 22:57:02 +0600

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <section className="bg-gray-900 text-white h-screen p-2">
+      <Home />
+    </section>
+  );
+};
+
+export default App;
